46765d86b192d27325794e73bf763cc506f6ebb4
kamilutkumavi0 <kamilutkumavi0@gmail.com>
2025-01-10 21:28:13 +0300

    new

--- src/toml_read.rs
+++ src/toml_read.rs
@@ -4,37 +4,14 @@ use home::home_dir;
 use std::fs;
 
 #[derive(Deserialize, Debug, Clone, Serialize)]
-pub enum ColorCutlie {
-    Black,
-    Red,
-    Green,
-    Yellow,
-    Blue,
-    Magenta,
-    Cyan,
-    White,
-    BrightBlack,
-    BrightRed,
-    BrightGreen,
-    BrightYellow,
-    BrightBlue,
-    BrightMagenta,
-    BrightCyan,
-    BrightWhite,
-}
-
-
-#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Command {
     name: String,
     logo: Option<String>,
     description: String,
-    color: ColorCutlie,
 }
 
 #[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Config {
-    bg_color: ColorCutlie,
     commands: Vec<Command>,
 }
 
