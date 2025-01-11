bdfc950510480b18375492cc3f68123e9039cd03
kamilutkumavi0 <kamilutkumavi0@gmail.com>
2025-01-11 19:19:43 +0300

    toml-read-done

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,1 @@
-
+pub mod toml_read;
--- src/toml_read.rs
+++ src/toml_read.rs
@@ -15,17 +15,30 @@ pub struct Config {
     commands: Vec<Command>,
 }
 
-pub fn toml_read(){
+pub enum ConfigError{
+    HomeDirNotFound,
+    CutlieNotInitialize,
+    ConfigTomlFormatError,
+}
+
+pub fn toml_read() -> Result<Config, ConfigError>{
     let home_diroctory = home_dir();
-    let config: Option<String> = match home_diroctory {
+    let config: String = match home_diroctory {
         Some(dir) => {
             let mut new_dir = dir.as_os_str().to_str().unwrap().to_string();
             new_dir.push_str("/.config/cutlie/config.toml");
             match fs::read_to_string(new_dir) {
-                Ok(f) => Some(f),
-                Err(_) => None,
+                Ok(f) => f,
+                Err(_) => return Err(ConfigError::CutlieNotInitialize),
             }
         }
-        None => None,
+        None => return Err(ConfigError::HomeDirNotFound),
     };
+    let config = match from_str(&config){
+        Ok(f) => f,
+        Err(e) => {
+            dbg!(&e);
+            return Err(ConfigError::ConfigTomlFormatError)},
+    };
+    Ok(config)
 }
