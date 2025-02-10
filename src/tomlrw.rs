6f0c08337b9b2ef5401c64e6abfabcbc0c8fcae0
Kamil Utku Mavi <114648102+kamilutkumavi0@users.noreply.github.com>
2025-02-10 13:49:36 +0300

    Update tomlrw.rs

--- src/tomlrw.rs
+++ src/tomlrw.rs
@@ -17,9 +17,9 @@ struct Config {
 }
 
 pub fn read(file_path: &str) -> Result<Config, io::Error> {
-    let mut file = File::open(file_path)?;
+    let mut file = File::open(file_path).unwrap();
     let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    file.read_to_string(&mut contents).unwrap();
     let config: Config = toml::from_str(&contents)?;
     Ok(config)
 }
